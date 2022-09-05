83a45fa0f279c8d57c05055c4516f8c22af01461
Victor Ly <ly.victor2000@gmail.com>
2022-09-05 14:37:36 +0200

    hex to rgb

--- script.js
+++ script.js
@@ -13,5 +13,20 @@ function init() {
 //get the selected color
 function selectColor() {
   let hexValue = this.value;
-  console.log(hexValue);
+  //console.log(hexValue);
+
+  let rgbValue = convertToRGB(hexValue.substring(hexValue.length - 6));
+  console.log(rgbValue);
+}
+
+//converts the color to rgb values
+function convertToRGB(hex) {
+  //console.log(hex);
+  let red = parseInt("0x" + hex.substring(0, 2));
+  let green = parseInt("0x" + hex.substring(2, 4));
+  let blue = parseInt("0x" + hex.substring(4, 6));
+
+  let rgb = `${red}, ${green}, ${blue}`;
+
+  return rgb;
 }
