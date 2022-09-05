4357b56a61382f06681c6b25365ceea0a1a659b4
Victor Ly <ly.victor2000@gmail.com>
2022-09-05 15:13:55 +0200

    hex to hsl

--- script.js
+++ script.js
@@ -12,21 +12,81 @@ function init() {
 
 //get the selected color
 function selectColor() {
-  let hexValue = this.value;
+  const hexValue = this.value.substring(this.value.length - 6);
   //console.log(hexValue);
 
-  let rgbValue = convertToRGB(hexValue.substring(hexValue.length - 6));
-  console.log(rgbValue);
+  const rgbValue = convertToRGB(hexValue);
+  //console.log(rgbValue);
+
+  let hslValue = convertToHSL(hexValue);
+  console.log(hslValue);
 }
 
-//converts the color to rgb values
+//converts the hex value to rgb values
 function convertToRGB(hex) {
   //console.log(hex);
-  let red = parseInt("0x" + hex.substring(0, 2));
-  let green = parseInt("0x" + hex.substring(2, 4));
-  let blue = parseInt("0x" + hex.substring(4, 6));
+  const red = parseInt("0x" + hex.substring(0, 2));
+  const green = parseInt("0x" + hex.substring(2, 4));
+  const blue = parseInt("0x" + hex.substring(4, 6));
 
-  let rgb = `${red}, ${green}, ${blue}`;
+  const rgb = `${red}, ${green}, ${blue}`;
 
   return rgb;
+}
+
+//converts the hex value to hsl values
+function convertToHSL(hex) {
+  const red = parseInt("0x" + hex.substring(0, 2)) / 255;
+  const green = parseInt("0x" + hex.substring(2, 4)) / 255;
+  const blue = parseInt("0x" + hex.substring(4, 6)) / 255;
+
+  let h, s, l;
+
+  const min = Math.min(red, green, blue);
+  const max = Math.max(red, green, blue);
+
+  switch (max) {
+    case min:
+      h = 0;
+      break;
+
+    case red:
+      h = 60 * (0 + (green - blue) / (max - min));
+      break;
+
+    case green:
+      h = 60 * (2 + (blue - red) / (max - min));
+      break;
+
+    case blue:
+      h = 60 * (4 + (red - green) / (max - min));
+      break;
+  }
+
+  if (h < 0) {
+    h = h + 360;
+  }
+
+  //calculate luminance/brightness
+  l = (min + max) / 2;
+
+  //calculate saturation
+  if (max === 0 || min === 1) {
+    s = 0;
+  } else {
+    s = (max - l) / Math.min(l, 1 - l);
+  }
+
+  //convert value to percent
+  s *= 100;
+  l *= 100;
+
+  //round values
+  h = Math.round(h);
+  s = Math.round(s);
+  l = Math.round(l);
+
+  const hsl = `${h}, ${s}, ${l}`;
+
+  return hsl;
 }
