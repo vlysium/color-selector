8ee21b9c298b40158f2766167f736a1479debeba
Victor Ly <ly.victor2000@gmail.com>
2022-09-05 14:18:50 +0200

    output selected color

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use script";
+
+const HTML = [];
+
+document.addEventListener("DOMContentLoaded", init);
+
+//initialize
+function init() {
+  HTML.colorPicker = document.getElementById("color-picker"); //color input element
+  HTML.colorPicker.addEventListener("input", selectColor);
+}
+
+//get the selected color
+function selectColor() {
+  let hexValue = this.value;
+  console.log(hexValue);
+}
